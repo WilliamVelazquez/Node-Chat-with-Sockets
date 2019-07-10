f20e8fb36d6803c5ac36b7f7c45322728309f7d3
William Velazquez <william.velazquez.a@gmail.com>
2019-07-10 16:57:08 -0500

    Updating Form Events and Styles, Adding ScrollToBottom, Preventing Submit to Refresh, Updating NotifyUser Feature and Adding favicon

--- client/client.js
+++ client/client.js
@@ -1,10 +1,17 @@
 //@Author William E. Velázquez Amarra - williamvelazquez.isc@gmail.com
 const socket = io(); 
+const KEY_CODES = [8,9,13,16,17,18,19,20,27,33,34,35,36,37,38,39,40,45,46,91,92,93,112,113,114,115,116,117,118,119,120,121,122,123,144,145,255];
 
 let USER;
 let $MESSAGE;
 let $MESSAGES_UNORDERED_LIST;
 let $NOTIFY_USER;
+
+let typingTimeOut;
+
+const scrollToBottom = function(){
+  window.scrollTo(0,document.body.scrollHeight);
+}
 
 const callback = function(){
   let name = makeid();
@@ -32,18 +39,21 @@ if (document.readyState === "complete" || (document.readyState !== "loading" && 
   document.addEventListener("DOMContentLoaded", callback);
 }
 
-const submitfunction = function(){
+const submitfunction = function(event){
+  event.preventDefault();
   let message=$MESSAGE.value;
   if(message != '') {
     socket.emit('chatMessage', USER, message);
   }
   $MESSAGE.value="";
   $MESSAGE.focus();
-  return false;
+  // return false;
 }
 
-const notifyTyping = function(){
-  socket.emit('notifyUser', USER);
+const notifyTyping = function(event){
+  if(!KEY_CODES.includes(event.keyCode)){
+    socket.emit('notifyUser', USER);
+  }
 }
 
 ////////////////////////////////////////
@@ -54,11 +64,17 @@ socket.on('chatMessage', function(from, msg){
   let color = (from == USER) ? 'green' : '#009afd';
   let fromText = (from == USER) ? 'Me' : from;
   $MESSAGES_UNORDERED_LIST.insertAdjacentHTML('beforeend','<li><b style="color:' + color + '">' + fromText + '</b>: ' + msg + '</li>');
+  scrollToBottom();
+  if(from !== USER){
+    $NOTIFY_USER.innerHTML="";
+    clearTimeout(typingTimeOut);
+  }
 });
 
 socket.on('notifyUser', function(user){
   if(user != USER) {
+    clearTimeout(typingTimeOut);
     $NOTIFY_USER.innerHTML=user + ' is typing ...';
   }
-  setTimeout(function(){$NOTIFY_USER.innerHTML=""; }, 10000);;
+  typingTimeOut=setTimeout(function(){$NOTIFY_USER.innerHTML=""; }, 5000);;
 });
